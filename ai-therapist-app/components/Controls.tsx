a32bbee5cd5c07397e3919b1d2bd94ad937a8238
Nick Faylor <nicholasfaylor2003@gmail.com>
2024-08-07 14:59:02 -0700

    integrated EVI API

---
+++ ai-therapist-app/components/Controls.tsx
@@ -0,0 +1,34 @@
+// ./components/Controls.tsx
+"use client";
+import { useVoice, VoiceReadyState } from "@humeai/voice-react";
+export default function Controls() {
+    const { connect, disconnect, readyState } = useVoice();
+
+    if (readyState === VoiceReadyState.OPEN) {
+        return (
+            <button
+                onClick={() => {
+                    disconnect();
+                }}
+            >
+                End Session
+            </button>
+        );
+    }
+
+    return (
+        <button
+            onClick={() => {
+                connect()
+                    .then(() => {
+                        /* handle success */
+                    })
+                    .catch(() => {
+                        /* handle error */
+                    });
+            }}
+        >
+            Start Session
+        </button>
+    );
+}
