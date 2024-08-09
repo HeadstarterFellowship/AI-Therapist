3f94c06fddb254d0abab2e75b101e47fefbc096a
TimothyPhan2 <98495706+TimothyPhan2@users.noreply.github.com>
2024-08-08 17:17:19 -0700

    Got hume test to work for now

---
+++ ai-therapy/components/ClientComponent.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { VoiceProvider } from "@humeai/voice-react";
+import Messages from "./Controls";
+import Controls from "./Messages";
+
+export default function ClientComponent({
+    accessToken,
+    configId,
+}: {
+    accessToken: string;
+    configId: string;
+}) {
+    return (
+        <VoiceProvider
+            auth={{ type: "accessToken", value: accessToken }}
+            configId={configId}
+        >
+            <Messages />
+            <Controls />
+        </VoiceProvider>
+    );
+}
