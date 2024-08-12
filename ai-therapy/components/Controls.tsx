d5daeacef7b743f233b91b27aae96b5e7094fafc
TimothyPhan2 <98495706+TimothyPhan2@users.noreply.github.com>
2024-08-11 23:19:30 -0700

    Update Controls.tsx

--- ai-therapy/components/Controls.tsx
+++ ai-therapy/components/Controls.tsx
@@ -11,10 +11,10 @@ import {
 } from "lucide-react";
 import { AnimatePresence, motion } from "framer-motion";
 import { Toggle } from "./ui/toggle";
-
+import MicFFT from "./MicFFT";
 import { cn } from "@/lib/utils";
 import UserInput from "./UserInput";
-
+import { useState } from "react";
 export default function Controls() {
   const { disconnect, status, isMuted, unmute, mute, connect } = useVoice();
 
@@ -31,7 +31,7 @@ export default function Controls() {
   return (
     <div
       className={cn(
-        "absolute bottom-0 left-0 w-full p-2 flex  items-center justify-center my-4"
+        "fixed bottom-0 left-0 w-full p-2 flex flex-col items-center justify-center"
       )}
     >
       <AnimatePresence>
@@ -49,60 +49,54 @@ export default function Controls() {
             opacity: 0,
           }}
           className={
-            "xs:p-2 m-2 p-4 bg-card border border-border rounded-lg shadow-sm flex flex-col items-center xs:gap-2 gap-4 bg-accent-700 "
+            "p-4 bg-card border border-border rounded-lg shadow-sm flex items-center gap-4 bg-accent-700 "
           }
         >
-          <div className="flex flex-row justify-center items-center">
           <UserInput />
           <Toggle
-              className={"bg-transparent"}
-              pressed={!isMuted}
-              onPressedChange={() => {
-                if (isMuted) {
-                  unmute();
-                } else {
-                  mute();
-                }
-              }}
-            >
-              
-              {isMuted ? (
-                <MicOff className={"xs:size-2 size-4 w-5 h-5 "} />
-              ) : (
-                <Mic className={"xs:size-2 size-4 w-5 h-5"} />
-              )}
-            </Toggle>
-          </div>
-          <div className="flex flex-row">
-            
+            className={"bg-transparent"}
+            pressed={!isMuted}
+            onPressedChange={() => {
+              if (isMuted) {
+                unmute();
+              } else {
+                mute();
+              }
+            }}
+          >
+            {isMuted ? (
+              <MicOff className={"size-4 w-5 h-5 "} />
+            ) : (
+              <Mic className={"size-4 w-5 h-5"} />
+            )}
+          </Toggle>
 
-            <Button
-              className={`flex items-center gap-1 ${
-                status.value === "connected" ? "" : "bg-secondary-700"
-              }`}
-              onClick={handleToggleConnection}
-              variant={status.value === "connected" ? "destructive" : "default"}
-            >
-              <span>
-                {status.value === "connected" ? (
-                  <MessageCircleOff
-                    className={"size-4 opacity-50"}
-                    strokeWidth={2}
-                    stroke={"currentColor"}
-                  />
-                ) : (
-                  <MessageCircleIcon
-                    className={"size-4 opacity-50"}
-                    strokeWidth={2}
-                    stroke={"currentColor"}
-                  />
-                )}
-              </span>
-              <span>
-                {status.value === "connected" ? "End Chat" : "Start Chat"}
-              </span>
-            </Button>
-          </div>
+          <Button
+            className={`flex items-center gap-1 ${
+              status.value === "connected" ? "" : "bg-green-700"
+            }`}
+            onClick={handleToggleConnection}
+            variant={status.value === "connected" ? "destructive" : "default"}
+          >
+            <span>
+              {status.value === "connected" ? (
+                <MessageCircleOff
+                  className={"size-4 opacity-50"}
+                  strokeWidth={2}
+                  stroke={"currentColor"}
+                />
+              ) : (
+                <MessageCircleIcon
+                  className={"size-4 opacity-50"}
+                  strokeWidth={2}
+                  stroke={"currentColor"}
+                />
+              )}
+            </span>
+            <span>
+              {status.value === "connected" ? "End Chat" : "Start Chat"}
+            </span>
+          </Button>
         </motion.div>
       </AnimatePresence>
     </div>
